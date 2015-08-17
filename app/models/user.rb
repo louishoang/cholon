703c32fcd69551964d96ef0093900293c943b6bf
Louis Hoang <bawsing@gmail.com>
2015-08-16 21:20:04 -0700

    user login with facebook and google done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,42 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :omniauthable
+
+  #find or create new account from omniauth response
+  def self.from_omniauth(auth_hash)
+    User.find_or_create_by(uid: auth_hash[:uid], provider: auth_hash[:provider]) do |user|
+      user.email = auth_hash[:info][:email]
+      user.avatar_url = auth_hash[:info][:image]
+      user.first_name = auth_hash[:info][:first_name]
+      user.last_name = auth_hash[:info][:last_name]
+    end
+  end
+
+  # if omniauth fails to create user on first try,
+   # it will be saved in session,
+    # then re-use those info to create user
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"], without_protection: true) do |user|
+        user_attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  #override login ask for password when using omniauth
+  def password_required?
+    super && provider.blank?
+  end
+
+  #override update information with password for omniauth
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      update_attributes(params, *options)
+    else
+      super
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150816231525) do
+ActiveRecord::Schema.define(version: 20150817005243) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -74,6 +74,11 @@ ActiveRecord::Schema.define(version: 20150816231525) do
     t.datetime "confirmation_sent_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.text     "avatar_url"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
