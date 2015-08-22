73d8cef5d1332aa53bfea0f9c5ff373244c71d8e
Louis Hoang <bawsing@gmail.com>
2015-08-22 16:53:27 -0700

    fixed login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper ApplicationHelper
-
+  helper_method :signed_in?, :current_user, :same_user?, :owner?
 
   #Set up multi-locale filter
   before_action :set_locale
