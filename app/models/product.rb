c2132b5b458fb82ec25e4308a9c58f7dbe399b0f
Louis Hoang <bawsing@gmail.com>
2015-08-22 22:40:13 -0700

    added more input to sell an item

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
+  CONDITION_USED = "Used"
+  CONDITION_NEW = "New"
+  CONDITION_REFURBISHED = "Refurbished"
+  CONDITION_FOR_PART_OR_NOT_WORKING = "For Part or Not Working"
+
   has_many :product_categories
   has_many :categories, through: :product_categories
   belongs_to :user
@@ -11,5 +16,10 @@ class Product < ActiveRecord::Base
   validates :stock_quantity, presence: true
   validates :user_id, presence: true
 
-
+  def self.condition_select_option
+    [[CONDITION_NEW, CONDITION_NEW],
+    [CONDITION_USED, CONDITION_USED],
+    [CONDITION_REFURBISHED, CONDITION_REFURBISHED],
+    [CONDITION_FOR_PART_OR_NOT_WORKING, CONDITION_FOR_PART_OR_NOT_WORKING]]
+  end
 end
