94f6683d782dd4697ddb06e4b5f1146b4339f532
Louis Hoang <bawsing@gmail.com>
2015-10-04 11:29:25 -0700

    fixed test , all passed

--- spec/factories/product_variant.rb
+++ spec/factories/product_variant.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     name Faker::Commerce.color
     price Faker::Commerce.price
     stock_quantity Faker::Number.between(1, 100)
+    is_default false
 
     after(:create) do |variant|
       FactoryGirl.create(:product_photo, :product_variant => variant)
