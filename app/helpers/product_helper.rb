9341ad005aefc0c2ae930113e84975e2879510f3
Louis Hoang <bawsing@gmail.com>
2016-01-28 17:18:41 -0800

    fixed query by multiple params value

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -7,14 +7,6 @@ module ProductHelper
     price = params[:max_price] || @max_price
   end
 
-  def merge_params(params=nil, new_attr)
-    if params.present?
-      "#{params},#{new_attr}"
-    else
-      new_attr
-    end
-  end
-
   def current_category_facet(search_resp)
     search_resp.facet(:category_ids).rows.map{|x| [x.value, x.count]} rescue nil
   end
@@ -25,5 +17,29 @@ module ProductHelper
 
   def current_shipping_method_facet(search_resp)
     search_resp.facet(:shipping_method).rows.map{|x| [x.value, x.count]} rescue nil
+  end
+
+  def merge_params_if_existed(params, ptype, value)
+    list = params[ptype].split(",") rescue nil
+    if list.blank?
+      url_for(params.merge({:category => value.to_s}))
+    elsif list.present? && !list.include?(value.to_s)
+      list << value.to_s
+      url_for(params.merge({:category => list.join(",")}))
+    else
+      url_for(params)
+    end
+  end
+
+  def dhskhfdskjf_params(params, ptype, value)
+    list = params[ptype].split(",") rescue nil
+    if list.present? && list.size == 1
+      url_for(params.except(ptype))
+    elsif list.present?
+      _list = list; _params = params.clone #prevent direct params change
+      _list.delete(value.to_s)
+      _params[ptype] = _list.join(",")
+      url_for(_params)
+    end
   end
 end
