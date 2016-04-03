746365055ed43ace37122b0ce14673468b7ae5c0
Louis Hoang <bawsing@gmail.com>
2016-04-02 22:26:12 -0700

    fix layout issue with owl carousel on small screen and modify home page css

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -80,8 +80,11 @@ var renderUI = function(cx){
       navigation : true, // Show next and prev buttons
       slideSpeed : 300,
       paginationSpeed : 400,
-      singleItem:true
- 
+      responsive:{
+        0: { items:1 },
+        600: { items:1 },
+        1000: { items:1 }
+        }
       // "singleItem:true" is a shortcut for:
       // items : 1, 
       // itemsDesktop : false,
